333d3705ba45f8ef51d7ef696a74f3ab6b827c28
takao.yamasaki <captain_kaeru@yahoo.co.jp>
2020-05-02 14:32:17 +0900

    編集機能と削除機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,19 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
     end
 
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        user = User.find(params[:id])
+        user.update(user_params)
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        user.delete
+    end
     private
     def user_params
         params.require(:user).permit(:name,:age)
